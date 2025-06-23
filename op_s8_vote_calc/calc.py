e9ac7b08af4b92edc5472c91fca66e5bebc5acaf
Jeff <jeff@voteagora.com>
2025-06-23 10:58:52 -0400

    split basic and approval

--- op_s8_vote_calc/calc.py
+++ op_s8_vote_calc/calc.py
@@ -16,6 +16,9 @@ from eth_abi import decode as decode_abi
 
 from abifsm import ABISet, ABI
 from .utils import camel_to_snake
+from .calc_basic import OffChainBasicMixin, OnChainBasicMixin,                     BasicTally,    FinalBasicTally
+from .calc_approval import OffChainApprovalMixin, OnChainApprovalMixin, Choice, ApprovalTally, FinalApprovalTally
+
 from .signatures import *
 
 from .attestations import meta as all_meta
@@ -59,247 +62,8 @@ class Proposal:
         self.proposal_type_id = onchain_meta['proposal_type_id']
         self.counting_mode = onchain_meta['counting_mode']
 
-def bte(x):
-    return "✅" if x else "❌"
 
-
-class StandardTally:
-
-    def __init__(self, eligible_votes, quorum_thresh_pct, approval_thresh_pct, against_votes=0, for_votes=0, abstain_votes=0, include_abstain=False):
-        """
-        eligible_votes: Number of eligible voters (for off-chain) or Votable Supply (for on-chain)
-        
-        quorum_thresh_pct: Quorum threshold percentage as a float.  Eg. 0.5 for 50%
-        approval_thresh_pct: Approval threshold percentage as a float.  Eg. 0.5 for 50%
-
-        against_votes: Number of against votes (for off-chain) or VP against (for on-chain)
-        for_votes: Number of for votes (for off-chain) or VP for (for on-chain)
-        abstain_votes: Number of abstain votes (for off-chain) or VP abstain (for on-chain)
-        """
-
-        self.for_votes = for_votes
-        self.against_votes = against_votes
-        self.abstain_votes = abstain_votes
-
-        self.include_abstain = include_abstain
-
-        if self.include_abstain:
-            self.total_votes = against_votes + for_votes + abstain_votes
-        else:
-            self.total_votes = against_votes + for_votes
-    
-        self.eligible_votes = eligible_votes
-
-        self.quorum_thresh_pct = quorum_thresh_pct
-        self.approval_thresh_pct = approval_thresh_pct
-
-        if self.total_votes != 0:
-
-            assert self.eligible_votes != 0, "eligible_votes must be non-zero, there should be no reason it's not."
-
-            self.relative_for_pct = self.for_votes / self.total_votes
-            self.relative_against_pct = self.against_votes / self.total_votes
-            self.relative_abstain_pct = self.abstain_votes / self.total_votes
-
-            self.absolute_for_pct = self.for_votes / self.eligible_votes
-            self.absolute_against_pct = self.against_votes / self.eligible_votes
-            self.absolute_abstain_pct = self.abstain_votes / self.eligible_votes
-
-            self.quorum = self.total_votes / self.eligible_votes
-            self.passing_quorum = self.quorum >= self.quorum_thresh_pct
-
-            self.approval = self.for_votes / self.total_votes
-            self.passing_approval_threshold = self.approval >= self.approval_thresh_pct
-
-        else:
-            self.relative_for_pct = 0
-            self.relative_against_pct = 0
-            self.relative_abstain_pct = 0
-            self.absolute_for_pct = 0
-            self.absolute_against_pct = 0
-            self.absolute_abstain_pct = 0
-            self.quorum = 0
-            self.approval = 0
-
-            self.passing_quorum = self.quorum >= self.quorum_thresh_pct
-            self.passing_approval_threshold = self.approval >= self.approval_thresh_pct
-
-    def gen_tally_report(self, label, weight=1):
-
-        tally = f"{label} Tally"
-        
-        if weight < 1:
-            tally += f" [{weight:.2%} of Final]\n"
-        else:
-            tally += "\n"
-
-        tally += "-" * (len(tally) - 1) + "\n"
-
-
-        tally += f"Total:    {self.total_votes:>32}\n"
-        tally += f"Eligible: {self.eligible_votes:>32}\n"
-
-        tally += f"For:      {self.for_votes:>32} ({self.relative_for_pct:.1%} of total | {self.absolute_for_pct:.1%} of eligible)\n"
-        tally += f"Against:  {self.against_votes:>32} ({self.relative_against_pct:.1%} of total | {self.absolute_against_pct:.1%} of eligible)\n"
-
-        if self.include_abstain:
-            tally += f"Abstain:  {self.abstain_votes:>32} ({self.relative_abstain_pct:.1%} of total | {self.absolute_abstain_pct:.1%} of eligible)\n"
-
-        tally += f"Quorum: {self.quorum:.2%} {bte(self.passing_quorum)} ({self.quorum_thresh_pct:.0%}), Approval: {self.approval:.2%} {bte(self.passing_approval_threshold)} ({self.approval_thresh_pct:.0%}) -> "
-
-        if self.passing_quorum and self.passing_approval_threshold:
-            tally += "✅ PASSING\n"
-        else:
-            tally += "❌ DEFEATED\n"
-
-        return tally
-
-
-class Choice(StandardTally):
-
-    def __init__(self, label, eligible_votes, quorum_thresh_pct, approval_thresh_pct, votes, include_abstain=False):
-
-        self.label = label
-
-        against_votes = int(votes.get(0, 0))
-        for_votes = int(votes.get(1, 0))
-        abstain_votes = int(votes.get(2, 0)) if include_abstain else 0
-
-        super().__init__(eligible_votes, quorum_thresh_pct, approval_thresh_pct, against_votes, for_votes, abstain_votes, include_abstain)
-
-
-class ApprovalTally:
-
-    def __init__(self, eligible_votes, quorum_thresh_pct, approval_thresh_pct, counts, total_voting_vp, include_abstain=False):
-        """
-        eligible_votes: Number of eligible voters (for off-chain) or Votable Supply (for on-chain)
-        
-        quorum_thresh_pct: Quorum threshold percentage as a float.  Eg. 0.5 for 50%
-        approval_thresh_pct: Approval threshold percentage as a float.  Eg. 0.5 for 50%
-
-        votes: Dictionary of choices with votes, where the key is the choice, and then sub-keys are for
-        against, for, abstain.  eg.
-
-        {'choice 1' : {'0' : 10, '1' : 20, '2' : 30},
-         'choice 2' : {'0' : 10, '1' : 20, '2' : 30}}
-
-        """
-
-        self.choice_tallies = {k : Choice(k, eligible_votes, quorum_thresh_pct, approval_thresh_pct, v, include_abstain) for k, v in counts.items()}
-
-        self.include_abstain = include_abstain
-
-        if self.include_abstain:
-            self.total_votes = sum([int(v) for v in total_voting_vp.values()])
-        else:
-            self.total_votes = sum([int(v) for k, v in total_voting_vp.items() if k in [0, 1]])
-    
-        self.eligible_votes = eligible_votes
-
-        self.quorum_thresh_pct = quorum_thresh_pct
-        self.approval_thresh_pct = approval_thresh_pct
-
-        if self.total_votes != 0:
-
-            assert self.eligible_votes != 0, "eligible_votes must be non-zero, there should be no reason it's not."
-
-            self.relative_pct = {k : {support : int(vp) / self.total_votes for support, vp in v.items()} for k, v in counts.items()}
-
-            self.absolute_pct = {k : {support : int(vp) / self.eligible_votes for support, vp in v.items()} for k, v in counts.items()}
-
-            self.quorum = self.total_votes / self.eligible_votes
-            self.passing_quorum = self.quorum >= self.quorum_thresh_pct
-
-            self.approval = {k : int(v.get(1, 0)) / self.total_votes for k, v in counts.items()}
-            self.passing_approval_threshold = {k : a >= self.approval_thresh_pct for k, a in self.approval.items()}
-
-        else:
-            self.relative_for_pct = 0
-            self.relative_against_pct = 0
-            self.relative_abstain_pct = 0
-            self.absolute_for_pct = 0
-            self.absolute_against_pct = 0
-            self.absolute_abstain_pct = 0
-            self.quorum = 0
-            self.approval = 0
-
-            self.passing_quorum = self.quorum >= self.quorum_thresh_pct
-            self.passing_approval_threshold = self.approval >= self.approval_thresh_pct
-
-    def gen_tally_report(self, label, weight=1):
-
-        tally = f"{label} Tally"
-        
-        if weight < 1:
-            tally += f" [{weight:.2%} of Final]\n"
-        else:
-            tally += "\n"
-
-        tally += "-" * (len(tally) - 1) + "\n"
-
-
-        tally += f"Given {self.total_votes} total votes, and {self.eligible_votes} eligible votes... \n"
-
-        for k, choice in self.choice_tallies.items():
-            if choice.label == -1 and self.include_abstain:
-                tally += f"Abstain: {choice.abstain_votes} ({choice.abstain_votes / self.total_votes:.1%} of total | {choice.absolute_abstain_pct:.1%} of eligible)\n"
-            else:
-                tally += f"For: {choice.for_votes} {bte(self.passing_approval_threshold[choice.label])} ({self.approval[choice.label]:.1%} of total | {choice.absolute_for_pct:.1%} of eligible)\n"
-        
-        tally += f"Quorum: {self.quorum:.2%} {bte(self.passing_quorum)} ({self.quorum_thresh_pct:.0%})"
-
-        return tally
-
-
-
-
-class FinalTally:
-    def __init__(self, tallies, weights, quorum_thresh_pct, approval_thresh_pct, include_abstain=False):
-        self.tallies = tallies
-        self.weights = weights
-
-        self.relative_for_pct = sum([t.relative_for_pct * w for t, w in zip(tallies, weights)])
-        self.relative_against_pct = sum([t.relative_against_pct * w for t, w in zip(tallies, weights)])
-        self.relative_abstain_pct = sum([t.relative_abstain_pct * w for t, w in zip(tallies, weights)])
-
-        self.absolute_for_pct = sum([t.absolute_for_pct * w for t, w in zip(tallies, weights)])
-        self.absolute_against_pct = sum([t.absolute_against_pct * w for t, w in zip(tallies, weights)])
-        self.absolute_abstain_pct = sum([t.absolute_abstain_pct * w for t, w in zip(tallies, weights)])
-
-        self.quorum = sum([t.quorum * w for t, w in zip(tallies, weights)])
-        self.approval = sum([t.approval * w for t, w in zip(tallies, weights)])
-
-        self.quorum_thresh_pct = quorum_thresh_pct
-        self.approval_thresh_pct = approval_thresh_pct
-   
-        self.passing_quorum = self.quorum >= quorum_thresh_pct
-        self.passing_approval_threshold = self.approval >= approval_thresh_pct     
-
-        self.include_abstain = include_abstain
-
-
-    def gen_tally_report(self, label):
-
-        tally = f"{label} Tally\n"
-        tally += "-" * (len(tally) - 1) + "\n"
-
-        tally += f"For: ({self.relative_for_pct:.1%} of total | {self.absolute_for_pct:.1%} of eligible)\n"
-        tally += f"Against: ({self.relative_against_pct:.1%} of total | {self.absolute_against_pct:.1%} of eligible)\n"
-
-        if self.include_abstain:
-            tally += f"Abstain: ({self.relative_abstain_pct:.1%} of total | {self.absolute_abstain_pct:.1%} of eligible)\n"
-
-        tally += f"Quorum: {self.quorum:.3%} {bte(self.passing_quorum)} ({self.quorum_thresh_pct:.0%}), Approval: {self.approval:.3%} {bte(self.passing_approval_threshold)} ({self.approval_thresh_pct:.0%}) -> "
-
-        if self.passing_quorum and self.passing_approval_threshold:
-            tally += "✅ PASSING\n"
-        else:
-            tally += "❌ DEFEATED\n"
-
-        return tally
-
-
-class OffChain(Proposal):
+class OffChain(Proposal, OffChainBasicMixin, OffChainApprovalMixin):
     emoji = '⛓️‍💥'
     def __init__(self, row):
         self.row = row.to_dict()
@@ -350,7 +114,7 @@ class OffChain(Proposal):
 
         self.offc_votes = offc_votes_with_citizens        
 
-    def calculate_standard_tallies(self):
+    def calculate_basic_tallies(self):
         
         assert self.proposal_type_label == 'basic', f"Proposal type is not basic: {self.proposal_type_label}"
 
@@ -372,7 +136,7 @@ class OffChain(Proposal):
 
             eligible_votes = self.ch_counts[category]
             
-            tally = StandardTally(eligible_votes, quorum_thresh_pct, approval_thresh_pct, cat_counts[0], cat_counts[1], cat_counts[2], include_abstain=self.include_abstain)
+            tally = BasicTally(eligible_votes, quorum_thresh_pct, approval_thresh_pct, cat_counts[0], cat_counts[1], cat_counts[2], include_abstain=self.include_abstain)
             tallies.append(tally)
         
         return tallies
@@ -385,7 +149,7 @@ class OffChain(Proposal):
         print(self)
         print()
 
-        tallies = self.calculate_standard_tallies()
+        tallies = self.calculate_basic_tallies()
 
         print(tallies[0].gen_tally_report("Citizen House - Apps", weights[0]))
         print(tallies[1].gen_tally_report("Citizen House - Users", weights[1]))
@@ -398,10 +162,10 @@ class OffChain(Proposal):
             assert t.quorum_thresh_pct == quorum_thresh_pct, f"Quorum PCTs do not match: {t.quorum_thresh_pct} != {quorum_thresh_pct} for tally {i}"
             assert t.approval_thresh_pct == approval_thresh_pct, f"Approval Threshold PCTs do not match: {t.approval_thresh_pct} != {approval_thresh_pct} for tally {i}"
 
-        final_tally = FinalTally(tallies, weights = weights, quorum_thresh_pct = quorum_thresh_pct, approval_thresh_pct = approval_thresh_pct, include_abstain=self.include_abstain)
+        final_tally = FinalBasicTally(tallies, weights = weights, quorum_thresh_pct = quorum_thresh_pct, approval_thresh_pct = approval_thresh_pct, include_abstain=self.include_abstain)
         print(final_tally.gen_tally_report("Final"))
 
-class OnChain(Proposal):
+class OnChain(Proposal, OnChainBasicMixin, OnChainApprovalMixin):
     emoji = '⛓️'
     def __init__(self, row):
         self.row = row.to_dict()
@@ -421,87 +185,6 @@ class OnChain(Proposal):
 
         self.onc_votes = df
 
-    def calculate_basic_tally(self):
-        
-        assert self.proposal_type_label == 'basic', f"Proposal type is not basic: {self.proposal_type_label}"
-
-        def bigint_sum(arr):
-            return str(sum([int(o) for o in arr.values]))
-    
-        counts = self.onc_votes.groupby('support')['weight'].apply(bigint_sum).to_dict()
-        against_votes = int(counts.get(0, 0))
-        for_votes = int(counts.get(1, 0))
-        abstain_votes = int(counts.get(2, 0))
-
-        approval_thresh_pct = (self.proposal_type_info['approval_threshold_bps'] / 10000)
-        quorum_thresh_pct = (self.proposal_type_info['quorum_bps'] / 10000)
-        
-        votable_supply = self.votable_supply   
-        assert quorum_thresh_pct == self.quorum / votable_supply
-
-        return StandardTally(votable_supply, quorum_thresh_pct, approval_thresh_pct, against_votes, for_votes, abstain_votes, include_abstain=self.include_abstain)
-
-    def calculate_approval_tally(self):
-        
-        assert self.proposal_type_label == 'approval', f"Proposal type is not approval: {self.proposal_type_label}"
-
-        onc_votes = self.onc_votes.copy()
-
-        def params_decode(arr):
-            try:
-                return decode_abi(["uint256[]"], bytes.fromhex(arr))[0]
-            except TypeError as e:
-                return [-1]
-
-        onc_votes['params'] = onc_votes['params'].apply(params_decode)
-
-        def bigint_sum(arr):
-            return str(sum([int(o) for o in arr.values]))
-
-        ballot_feed = onc_votes[['support', 'weight', 'params']].explode('params')
-
-        counts = ballot_feed.groupby(['params', 'support']).apply(bigint_sum)
-        totals = ballot_feed.groupby(['params'])['weight'].apply(bigint_sum).to_dict()
-    
-        total_voting_vp = onc_votes[['support', 'weight']].groupby('support').apply(bigint_sum).to_dict()
-
-        final_dict = defaultdict(dict)
-        for (param, support), value in counts.items():
-            final_dict[param][support] = value
-        counts = dict(final_dict)
-
-        """
-        At this point we have "counts" of the form...
-
-        {-1: {2: '6515768095338571250010090'},
-        0: {0: '48664653865959285301', 1: '31431910661813432620160489'},
-        1: {1: '35035962495117270703561571'},
-        2: {0: '48664653865959285301', 1: '17033284171118898054256072'},
-        3: {1: '17663853612321229474897508'},
-        4: {0: '48664653865959285301', 1: '24934837283900123569914475'},
-        5: {1: '28476520065880757383430022'},
-        6: {0: '48664653865959285301', 1: '23272354186448414025424072'},
-        7: {1: '21779420495943287974126505'}}
-
-        and "total" is of the form...  ...but unclear if we really need these.
-
-        {-1: '6515768095338571250010090', 0: '31431959326467298579445790', 1: '35035962495117270703561571', 2: '17033332835772764013541373', 3: '17663853612321229474897508', 4: '24934885948553989529199776', 5: '28476520065880757383430022', 6: '23272402851102279984709373', 7: '21779420495943287974126505'}
-
-        and "total voting vp" is of the form...
-
-        {0: '48664653865959285301', 1: '40422986148598663804350243', 2: '6515768095338571250010090'}
-
-        """
-
-        approval_thresh_pct = (self.proposal_type_info['approval_threshold_bps'] / 10000)
-        quorum_thresh_pct = (self.proposal_type_info['quorum_bps'] / 10000)
-        
-        votable_supply = self.votable_supply   
-        assert quorum_thresh_pct == self.quorum / votable_supply
-
-        # TODO - should this really be "include_abstain=True"?
-        return ApprovalTally(votable_supply, quorum_thresh_pct, approval_thresh_pct, counts, total_voting_vp, include_abstain=True)
-
     def show_result(self):
 
         weights = [1]
@@ -515,7 +198,7 @@ class OnChain(Proposal):
 
             print(tally.gen_tally_report("Token House"))
             
-            final_tally = FinalTally([tally], weights = weights, quorum_thresh_pct = tally.quorum_thresh_pct, approval_thresh_pct = tally.approval_thresh_pct)
+            final_tally = FinalBasicTally([tally], weights = weights, quorum_thresh_pct = tally.quorum_thresh_pct, approval_thresh_pct = tally.approval_thresh_pct)
             print(final_tally.gen_tally_report("Final"))
 
         elif self.proposal_type_label == 'approval':
@@ -560,7 +243,7 @@ class Hybrid(Proposal):
 
         if self.proposal_type_label == 'basic':
             onc_tally = self.on_chain_p.calculate_basic_tally()
-            offc_tallies = self.off_chain_p.calculate_standard_tallies()
+            offc_tallies = self.off_chain_p.calculate_basic_tallies()
 
         elif self.proposal_type_label == 'approval':
             onc_tally = self.on_chain_p.calculate_approval_tally()
@@ -580,7 +263,7 @@ class Hybrid(Proposal):
             assert t.quorum_thresh_pct == quorum_thresh_pct, f"Quorum PCTs do not match: {t.quorum_thresh_pct} != {quorum_thresh_pct} for tally {i}"
             assert t.approval_thresh_pct == approval_thresh_pct, f"Approval Threshold PCTs do not match: {t.approval_thresh_pct} != {approval_thresh_pct} for tally {i}"
 
-        final_tally = FinalTally(tallies, weights = weights, quorum_thresh_pct = quorum_thresh_pct, approval_thresh_pct = approval_thresh_pct, include_abstain=self.include_abstain)
+        final_tally = FinalBasicTally(tallies, weights = weights, quorum_thresh_pct = quorum_thresh_pct, approval_thresh_pct = approval_thresh_pct, include_abstain=self.include_abstain)
         print(final_tally.gen_tally_report("Final"))
     
     def load_context(self):
---
+++ op_s8_vote_calc/calc_approval.py
@@ -0,0 +1,191 @@
+import click
+import csv, os
+from pathlib import Path
+from yaml import load, FullLoader
+from pprint import pprint
+import json
+from copy import deepcopy
+
+from .jsonrpc_client import JsonRpcHistHttpClient
+from .graphqleas_client import EASGraphQLClient
+from .utils import load_config, get_web3
+from web3 import Web3
+from collections import defaultdict
+
+from eth_abi import decode as decode_abi
+
+from abifsm import ABISet, ABI
+from .utils import camel_to_snake
+from .calc_basic import BasicTally
+from .signatures import *
+
+from .attestations import meta as all_meta
+
+import pandas as pd
+
+DATA_DIR = Path(os.getenv('S8_DATA_DIR', 'op_s8_vote_calc/data'))
+DEPLOYMENT = os.getenv('S8_DEPLOYMENT', 'test')
+
+onchain_config, offchain_config = load_config()
+PTC_MODULES = {v['address'] : v['name'] for v in onchain_config['gov']['modules']}
+
+class Choice(BasicTally):
+
+    def __init__(self, label, eligible_votes, quorum_thresh_pct, approval_thresh_pct, votes, include_abstain=False):
+
+        self.label = label
+
+        against_votes = int(votes.get(0, 0))
+        for_votes = int(votes.get(1, 0))
+        abstain_votes = int(votes.get(2, 0)) if include_abstain else 0
+
+        super().__init__(eligible_votes, quorum_thresh_pct, approval_thresh_pct, against_votes, for_votes, abstain_votes, include_abstain)
+
+
+class ApprovalTally:
+
+    def __init__(self, eligible_votes, quorum_thresh_pct, approval_thresh_pct, counts, total_voting_vp, include_abstain=False):
+        """
+        eligible_votes: Number of eligible voters (for off-chain) or Votable Supply (for on-chain)
+        
+        quorum_thresh_pct: Quorum threshold percentage as a float.  Eg. 0.5 for 50%
+        approval_thresh_pct: Approval threshold percentage as a float.  Eg. 0.5 for 50%
+
+        votes: Dictionary of choices with votes, where the key is the choice, and then sub-keys are for
+        against, for, abstain.  eg.
+
+        {'choice 1' : {'0' : 10, '1' : 20, '2' : 30},
+         'choice 2' : {'0' : 10, '1' : 20, '2' : 30}}
+
+        """
+
+        self.choice_tallies = {k : Choice(k, eligible_votes, quorum_thresh_pct, approval_thresh_pct, v, include_abstain) for k, v in counts.items()}
+
+        self.include_abstain = include_abstain
+
+        if self.include_abstain:
+            self.total_votes = sum([int(v) for v in total_voting_vp.values()])
+        else:
+            self.total_votes = sum([int(v) for k, v in total_voting_vp.items() if k in [0, 1]])
+    
+        self.eligible_votes = eligible_votes
+
+        self.quorum_thresh_pct = quorum_thresh_pct
+        self.approval_thresh_pct = approval_thresh_pct
+
+        if self.total_votes != 0:
+
+            assert self.eligible_votes != 0, "eligible_votes must be non-zero, there should be no reason it's not."
+
+            self.relative_pct = {k : {support : int(vp) / self.total_votes for support, vp in v.items()} for k, v in counts.items()}
+
+            self.absolute_pct = {k : {support : int(vp) / self.eligible_votes for support, vp in v.items()} for k, v in counts.items()}
+
+            self.quorum = self.total_votes / self.eligible_votes
+            self.passing_quorum = self.quorum >= self.quorum_thresh_pct
+
+            self.approval = {k : int(v.get(1, 0)) / self.total_votes for k, v in counts.items()}
+            self.passing_approval_threshold = {k : a >= self.approval_thresh_pct for k, a in self.approval.items()}
+
+        else:
+            self.relative_for_pct = 0
+            self.relative_against_pct = 0
+            self.relative_abstain_pct = 0
+            self.absolute_for_pct = 0
+            self.absolute_against_pct = 0
+            self.absolute_abstain_pct = 0
+            self.quorum = 0
+            self.approval = 0
+
+            self.passing_quorum = self.quorum >= self.quorum_thresh_pct
+            self.passing_approval_threshold = self.approval >= self.approval_thresh_pct
+
+    def gen_tally_report(self, label, weight=1):
+
+        tally = f"{label} Tally"
+        
+        if weight < 1:
+            tally += f" [{weight:.2%} of Final]\n"
+        else:
+            tally += "\n"
+
+        tally += "-" * (len(tally) - 1) + "\n"
+
+
+        tally += f"Given {self.total_votes} total votes, and {self.eligible_votes} eligible votes... \n"
+
+        for k, choice in self.choice_tallies.items():
+            if choice.label == -1 and self.include_abstain:
+                tally += f"Abstain: {choice.abstain_votes} ({choice.abstain_votes / self.total_votes:.1%} of total | {choice.absolute_abstain_pct:.1%} of eligible)\n"
+            else:
+                tally += f"For: {choice.for_votes} {bte(self.passing_approval_threshold[choice.label])} ({self.approval[choice.label]:.1%} of total | {choice.absolute_for_pct:.1%} of eligible)\n"
+        
+        tally += f"Quorum: {self.quorum:.2%} {bte(self.passing_quorum)} ({self.quorum_thresh_pct:.0%})"
+
+        return tally
+
+
+
+
+class OnChainApprovalMixin:
+    
+    def calculate_approval_tally(self):
+        
+        assert self.proposal_type_label == 'approval', f"Proposal type is not approval: {self.proposal_type_label}"
+
+        onc_votes = self.onc_votes.copy()
+
+        def params_decode(arr):
+            try:
+                return decode_abi(["uint256[]"], bytes.fromhex(arr))[0]
+            except TypeError as e:
+                return [-1]
+
+        onc_votes['params'] = onc_votes['params'].apply(params_decode)
+
+        def bigint_sum(arr):
+            return str(sum([int(o) for o in arr.values]))
+
+        ballot_feed = onc_votes[['support', 'weight', 'params']].explode('params')
+
+        counts = ballot_feed.groupby(['params', 'support']).apply(bigint_sum)
+        totals = ballot_feed.groupby(['params'])['weight'].apply(bigint_sum).to_dict()
+    
+        total_voting_vp = onc_votes[['support', 'weight']].groupby('support').apply(bigint_sum).to_dict()
+
+        final_dict = defaultdict(dict)
+        for (param, support), value in counts.items():
+            final_dict[param][support] = value
+        counts = dict(final_dict)
+
+        """
+        At this point we have "counts" of the form...
+
+        {-1: {2: '6515768095338571250010090'},
+        0: {0: '48664653865959285301', 1: '31431910661813432620160489'},
+        1: {1: '35035962495117270703561571'},
+        2: {0: '48664653865959285301', 1: '17033284171118898054256072'},
+        3: {1: '17663853612321229474897508'},
+        4: {0: '48664653865959285301', 1: '24934837283900123569914475'},
+        5: {1: '28476520065880757383430022'},
+        6: {0: '48664653865959285301', 1: '23272354186448414025424072'},
+        7: {1: '21779420495943287974126505'}}
+
+        and "total" is of the form...  ...but unclear if we really need these.
+
+        {-1: '6515768095338571250010090', 0: '31431959326467298579445790', 1: '35035962495117270703561571', 2: '17033332835772764013541373', 3: '17663853612321229474897508', 4: '24934885948553989529199776', 5: '28476520065880757383430022', 6: '23272402851102279984709373', 7: '21779420495943287974126505'}
+
+        and "total voting vp" is of the form...
+
+        {0: '48664653865959285301', 1: '40422986148598663804350243', 2: '6515768095338571250010090'}
+
+        """
+
+        approval_thresh_pct = (self.proposal_type_info['approval_threshold_bps'] / 10000)
+        quorum_thresh_pct = (self.proposal_type_info['quorum_bps'] / 10000)
+        
+        votable_supply = self.votable_supply   
+        assert quorum_thresh_pct == self.quorum / votable_supply
+
+        # TODO - should this really be "include_abstain=True"?
+        return ApprovalTally(votable_supply, quorum_thresh_pct, approval_thresh_pct, counts, total_voting_vp, include_abstain=True)
